1f842836ed8a89054b23d57386a19484ff0c7ba9
sergey.pylypyshko <sergeypylypyshko@gmail.com>
2026-03-24 17:11:57 +0200

    Add fix main screen amd calendar

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -75,6 +75,15 @@ compose.desktop {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = appPackageName
             packageVersion = "1.0.0"
+            macOS {
+                iconFile.set(project.file("../media/appicon/MacOsIcon.icns"))
+            }
+            windows {
+                iconFile.set(project.file("../media/appicon/WindowsIcon.ico"))
+            }
+            linux {
+                iconFile.set(project.file("../media/appicon/LinuxIcon.png"))
+            }
         }
     }
 }
